1aa01e49fe4addcda83e893b00bff39d9e01f44c
Iohel <joelpons9@hotmail.com>
2024-05-22 21:13:05 +0200

    Demo Final

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+let article = document.getElementsByClassName("Article");
+
+function Article(){
+    location.href="../html/article.html";
+}
+function Noticies() {
+    location.href="../index.html";
+}
+function Calendari() {
+    location.href="../html/calendari.html";
+}
+
+
